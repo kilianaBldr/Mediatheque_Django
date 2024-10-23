e8a2514f6b48a7da6be7e02b82edc6ac8aa24238
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-10-23 16:34:18 +0200

    ajout de l'app bibliothecaire

---
+++ bibliothecaire/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import liste_membres, creer_membre, mise_a_jour_membre, liste_medias, creer_media, creer_emprunt, retourner_emprunt
+
+
+urlpatterns = [
+    path('membres/', liste_membres, name='liste_membres'),
+    path('membres/creer/', creer_membre, name='creer_membre'),
+    path('membres/<int:id>/modifier/', mise_a_jour_membre, name='mise_a_jour_membre'),
+    path('medias/', liste_medias, name='liste_medias'),
+    path('medias/creer/', creer_media, name='creer_media'),
+    path('medias/<int:media_id>/emprunter/', creer_emprunt, name='creer_emprunt'),
+    path('emprunts/<int:emprunt_id>/retourner/', retourner_emprunt, name='retourner_emprunt'),
+]
