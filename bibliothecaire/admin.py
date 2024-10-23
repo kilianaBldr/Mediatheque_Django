455db53fb2b4bb561ef773eeb16c6ee3eece3d22
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-10-23 16:33:09 +0200

    Ajout des url et de l'admin de l'app bibliothecaire

--- bibliothecaire/admin.py
+++ bibliothecaire/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Membre, Media, Emprunt
 
-# Register your models here.
+class MembreAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenom', 'email', 'date_inscription')
+    search_fields = ('nom', 'prenom', 'email')
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'type_media', 'disponible')
+    list_filter = ('type_media', 'disponible')
+    search_fields = ('titre',)
+
+class EmpruntAdmin(admin.ModelAdmin):
+    list_display = ('media', 'membre', 'date_emprunt', 'date_retour')
+    list_filter = ('date_emprunt', 'date_retour')
+    search_fields = ('media__titre', 'membre__nom')
+
+admin.site.register(Membre, MembreAdmin)
+admin.site.register(Media, MediaAdmin)
+admin.site.register(Emprunt, EmpruntAdmin)
