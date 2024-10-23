7cb2615978f8b74ad944e983f5443f3033105b7d
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-10-23 16:39:46 +0200

    Ajout de la views.py de l'app membre

--- mediatheque/urls.py
+++ mediatheque/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bibliothecaire/', include('bibliothecaire.urls')),
+    path('', include('membre.urls')),
 ]
