8889aee55396c2587d46c0234f4d635d2c643f1f
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-10-23 18:33:40 +0200

    Ajout des tests emprunt de l'app bibliothecaire

--- bibliothecaire/tests.py
+++ bibliothecaire/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import Membre, Media
+from .models import Membre, Media, Emprunt
 
 
 class MembreTests(TestCase):
@@ -64,3 +64,28 @@ class MediaTests(TestCase):
         response = self.client.get(reverse('liste_medias'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Livre")
+
+
+class EmpruntTests(TestCase):
+
+    def setUp(self):
+        self.membre = Membre.objects.create(nom="Dupont", prenom="Jean", email="jean.dupont@example.com")
+        self.media = Media.objects.create(titre="Test Livre", type_media="Livre", disponible=True)
+        self.emprunt = Emprunt.objects.create(media=self.media, membre=self.membre)
+
+    def test_creer_emprunt(self):
+        # Test de la création d'un emprunt
+        response = self.client.post(reverse('creer_emprunt', args=[self.media.id]), {
+            'membre': self.membre.id
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Emprunt.objects.count(), 2) # Un nouvel emprunt a été créé
+        self.assertFalse(Media.objects.get(id=self.media.id).disponible)
+
+    def test_retourner_emprunt(self):
+        # Test pour vérifier que l'emprunt est bien retourné
+        response = self.client.post(reverse('retourner_emprunt', args=[self.emprunt.id]))
+        self.assertEqual(response.status_code, 302)
+        self.emprunt.refresh_from_db()
+        self.assertIsNotNone(self.emprunt.date_retour)
+        self.assertTrue(Media.objects.get(id=self.media.id).disponible)
